dc51fb9297e6930d45da2174bdeb8967cc7853c7
JohnPetersenDenmark <55278647+JohnPetersenDenmark@users.noreply.github.com>
2022-07-13 12:30:33 +0200

    My seriously commit

--- Docker/Program.cs
+++ Docker/Program.cs
@@ -12,7 +12,7 @@ namespace Docker
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        }
+        } 
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
