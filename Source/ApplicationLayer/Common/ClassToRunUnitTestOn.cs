ac4f47b8abd632240b41377dffba47973bb2afd9
JohnPetersenDenmark <55278647+JohnPetersenDenmark@users.noreply.github.com>
2022-07-16 00:24:02 +0200

    aaa

---
+++ Source/ApplicationLayer/Common/ClassToRunUnitTestOn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLayer.Common
+{
+    public class ClassToRunUnitTestOn
+    {
+
+        public int addTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- UnitTestFlexyBox/UnitTest1.cs
+++ UnitTestFlexyBox/UnitTest1.cs
@@ -1,13 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApplicationLayer.Common;
 
 namespace UnitTestFlexyBox
 {
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
+            ClassToRunUnitTestOn classToRunUnitTestOn = new ClassToRunUnitTestOn();
+
+            int x = 3;
+            int y = 4;
+            int res = classToRunUnitTestOn.addTwoNumbers(x,y);
             Assert.IsTrue(true);
         }
     }
