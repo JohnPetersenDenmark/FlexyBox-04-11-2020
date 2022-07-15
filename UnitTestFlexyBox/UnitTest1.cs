4f214d0d48a45dc069f6b59e56898244b1ee3adb
JohnPetersenDenmark <55278647+JohnPetersenDenmark@users.noreply.github.com>
2022-07-15 12:21:51 +0200

    qqq
    aaa

---
+++ UnitTestFlexyBox/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFlexyBox
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(true, "1 should not be prime");
+        }
+    }
+}
